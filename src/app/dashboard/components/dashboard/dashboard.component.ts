dd8d409b1a1634041cc398387b6577a083625a79
capraibex <32867660+capraibex@users.noreply.github.com>
2020-04-11 19:04:44 +0200

    split subscriptions

--- src/app/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/dashboard/components/dashboard/dashboard.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { DataService } from '../../services/data.service';
-import { forkJoin } from 'rxjs';
 import { IDataSeriesItem } from '../../directives/IDataSeriesItem';
-import { DataPoint } from '../../DataPoint';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,7 +9,7 @@ import { DataPoint } from '../../DataPoint';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  private data = {};
+  private data: {[type: string]: object} = {};
   public chartData: {[type: string]: IDataSeriesItem[]} = {};
 
   constructor(private dataService: DataService) { }
@@ -21,21 +19,12 @@ export class DashboardComponent implements OnInit {
   }
 
   fetchData() {
-    const simpleData$ = this.dataService.getSimpleData();
-    const metaData$ = this.dataService.getMetaData();
-    const all$ = this.dataService.getAll();
-    forkJoin([simpleData$, metaData$, ...all$]).subscribe(
-      ([simpleData, metaData, bundesland, bezirke, geschlechtsverteilung, altersverteilung, trend]) => {
-        this.data = {
-          ...simpleData, ...metaData, bundesland, bezirke, geschlechtsverteilung, altersverteilung, trend
-        };
-        this.chartData = {
-          altersverteilung: [{ type: 'column', color: '#e55400', dataPoints: altersverteilung as DataPoint[] }],
-          bundesland: [{ type: 'column', color: '#73bf69', dataPoints: bundesland as DataPoint[] }],
-          trend: [{ type: 'line', dataPoints: trend as DataPoint[] }],
-          geschlechtsverteilung: [{ type: 'pie', dataPoints: geschlechtsverteilung as DataPoint[] }],
-          bezirke: [{ type: 'table', dataPoints: bezirke as DataPoint[] }],
-        };
-    });
+    this.dataService.getBundesland().subscribe((r) => { this.chartData.bundesland = [{ type: 'column', color: '#73bf69', dataPoints: r }] });
+    this.dataService.getBezirke().subscribe((r) => { this.chartData.bezirke = [{ type: 'table', dataPoints: r }] });
+    this.dataService.getGeschlechtsverteilung().subscribe((r) => { this.chartData.geschlechtsverteilung = [{ type: 'pie', dataPoints: r }] });
+    this.dataService.getAltersverteilung().subscribe((r) => { this.chartData.altersverteilung = [{ type: 'column', color: '#e55400', dataPoints: r }] });
+    this.dataService.getTrend().subscribe((r) => { this.chartData.trend = [{ type: 'line', dataPoints: r }] });
+    this.dataService.getSimpleData().subscribe((r) => { this.data.simpleData = r });
+    this.dataService.getMetaData().subscribe((r) => { this.data.getMetaData = r });
   }
 }
