3473657c1b69e7339ac11e3a581ad277ae25b7d0
capraibex <32867660+capraibex@users.noreply.github.com>
2020-04-18 10:32:19 +0200

    add MatRadioModule to dashboard module

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,6 +5,7 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import { MatSortModule } from '@angular/material/sort';
+import { MatRadioModule } from '@angular/material/radio';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LocationTableComponent } from './components/location-table/location-table.component';
 import { MapComponent } from './components/map/map.component';
@@ -19,7 +20,8 @@ import { ChartDirective } from './directives/chart.directive';
     MatGridListModule,
     MatTableModule,
     MatCardModule,
-    MatSortModule
+    MatSortModule,
+    MatRadioModule
   ]
 })
 export class DashboardModule { }
